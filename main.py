4c6cc0905866a6d8a4d61da68b7dd29f6985da2e
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2026-05-10 17:33:58 -0300

    listagem das images v1

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def listar_container():
     for container in cliente.containers.list():
         print(container.id, container.status, container.name, container.ports, container.health)
 
+def listar_images():
+    cliente = criar_cliente()
+    for images in cliente.images.list():
+        print(images.id, images.labels)
 
 if __name__ == "__main__":
     listar_container()
