2101ad7e4e22c3d2462c527230f1ba71c4f1f7d6
ReinaldoLuizSilva <reinaldodasilva@ienh.com.br>
2026-05-10 10:23:59 -0300

    listagem dos containers v3

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from conexao import criar_cliente
 
 def listar_container():
     cliente = criar_cliente()
-    for conatiner in cliente.containers.list():
-        print(conatiner.id)
+    for container in cliente.containers.list():
+        print(container.id, container.status, container.name, container.ports, container.health)
 
 
 if __name__ == "__main__":
